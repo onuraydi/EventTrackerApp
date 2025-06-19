4448b3ec56dda6c1893b4dc9704d376623f45b72
Onur Aydı <onuraydi@outlook.com>
2025-06-19 09:14:11 +0300

    created screen

---
+++ app/src/main/java/com/example/eventtrackerapp/utils/EventAppOutlinedTextField.kt
@@ -0,0 +1,19 @@
+package com.example.eventtrackerapp.utils
+
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+
+@Composable
+fun EventAppOutlinedTextField(txt:String,state: MutableState<String>)
+{
+    OutlinedTextField(value = "${txt}",
+        onValueChange = {
+            state.value = it;
+        },
+        singleLine = true,
+        placeholder = {"${txt}"},
+        label = { Text(text = "${txt}") }
+    )
+}
