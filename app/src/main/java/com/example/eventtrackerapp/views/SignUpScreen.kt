c9d090ca2d51998e2b3fac0ae2079c62f41e3bf5
Onur Aydı <onuraydi@outlook.com>
2025-06-19 10:03:40 +0300

    modified utils

--- app/src/main/java/com/example/eventtrackerapp/views/SignUpScreen.kt
+++ app/src/main/java/com/example/eventtrackerapp/views/SignUpScreen.kt
@@ -16,7 +16,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -90,18 +92,30 @@ fun SignUpScreen()
                 modifier = Modifier.padding(top = 20.dp)
                 )
             Spacer(modifier = Modifier.padding(15.dp))
-            EventTrackerAppOutlinedTextField("Kullanıcı Adı", state = fullName);
+            EventTrackerAppOutlinedTextField("Kullanıcı Adı", state = fullName,
+                leadingIcon = {
+                    Icon(Icons.Filled.Person,"full name")
+                })
 
             Spacer(modifier = Modifier.padding(15.dp))
 
-            EventTrackerAppOutlinedTextField("Email", state = email);
+            EventTrackerAppOutlinedTextField("Email", state = email,
+                leadingIcon = {
+                    Icon(Icons.Filled.Email,"email")
+                });
 
             Spacer(modifier = Modifier.padding(15.dp))
 
-            EventTrackerAppOutlinedTextField("Şifre", state = password);
+            EventTrackerAppOutlinedTextField("Şifre", state = password,
+                leadingIcon = {
+                    Icon(Icons.Filled.Lock,"password")
+                });
             Spacer(modifier = Modifier.padding(15.dp))
 
-            EventTrackerAppOutlinedTextField("Şifre tekrarı", state = passwordConfirm);
+            EventTrackerAppOutlinedTextField("Şifre tekrarı", state = passwordConfirm,
+                leadingIcon = {
+                    Icon(Icons.Filled.Lock,"confirm password")
+            });
             Spacer(modifier = Modifier.padding(20.dp));
 
             Button(onClick = {},
