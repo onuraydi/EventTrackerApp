5d4ae2aa30ff7b8fbb9883e574c4580fc55d3ef4
Onur Aydı <onuraydi@outlook.com>
2025-06-20 11:23:21 +0300

    fixed conflict

--- app/src/main/java/com/example/eventtrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/eventtrackerapp/MainActivity.kt
@@ -7,13 +7,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.eventtrackerapp.ui.theme.EventTrackerAppTheme
+import com.example.eventtrackerapp.views.HomeScreen
 import com.example.eventtrackerapp.views.LoginScreen
+import com.example.eventtrackerapp.views.ProfileScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +31,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EventTrackerAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(Modifier.padding(innerPadding)) {
-                        LoginScreen()
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        NavigationBar(windowInsets = NavigationBarDefaults.windowInsets, modifier = Modifier) {
+                            NavigationBarItem(
+                                selected = true,
+                                icon ={ Icon(Icons.Filled.Home,"Home") },
+                                label = {Text("Home")},
+                                onClick = {},
+                            )
+
+                            NavigationBarItem(
+                                selected = false,
+                                icon ={ Icon(Icons.Filled.Search,"Explore") },
+                                label = {Text("Explore")},
+                                onClick = {},
+                            )
+
+                            NavigationBarItem(
+                                selected = false,
+                                icon ={ Icon(Icons.Filled.Person,"Profile") },
+                                label = {Text("Profile")},
+                                onClick = {},
+                            )
+                        }
                     }
+                ) { contentPadding -> Box(Modifier.padding(contentPadding))
+                    ProfileScreen()
                 }
             }
         }
