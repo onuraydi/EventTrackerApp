ccf25999ef7830552178b377c2617f6752242f14
Yavuz Selim Ozkan <ozkanyavuzselim99@gmail.com>
2025-06-24 15:13:02 +0300

    model updated.

--- app/src/main/java/com/example/eventtrackerapp/model/Tag.kt
+++ app/src/main/java/com/example/eventtrackerapp/model/Tag.kt
@@ -1,8 +1,11 @@
 package com.example.eventtrackerapp.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "tags")
 data class Tag(
-    val tagName:String
-)
+    val name:String
+){
+    @PrimaryKey(autoGenerate = true) var id:Int = 0
+}
