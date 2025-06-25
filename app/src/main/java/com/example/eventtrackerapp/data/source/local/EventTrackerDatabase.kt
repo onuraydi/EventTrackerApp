846d45ed3092cd867161997640c1b898a55f3e47
Onur Aydı <onuraydi@outlook.com>
2025-06-25 10:42:40 +0300

    fix: correct database annotations for Profile and Event models

--- app/src/main/java/com/example/eventtrackerapp/data/source/local/EventTrackerDatabase.kt
+++ app/src/main/java/com/example/eventtrackerapp/data/source/local/EventTrackerDatabase.kt
@@ -5,9 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.eventtrackerapp.model.Category
+import com.example.eventtrackerapp.model.Event
+import com.example.eventtrackerapp.model.Profile
 import com.example.eventtrackerapp.model.Tag
 
-@Database(entities = [Category::class, Tag::class], version = 1)
+@Database(entities = [Category::class, Tag::class, Event::class, Profile::class], version = 1)
 abstract class EventTrackerDatabase : RoomDatabase(){
 
     abstract fun eventDao(): EventDao
