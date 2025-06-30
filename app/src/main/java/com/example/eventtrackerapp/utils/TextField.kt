43c47aa1f8c33504687e6ffd791a30d885955fc5
Yavuz Selim Ozkan <ozkanyavuzselim99@gmail.com>
2025-06-30 16:11:24 +0300

    feat:added new common text field

--- app/src/main/java/com/example/eventtrackerapp/utils/TextField.kt
+++ app/src/main/java/com/example/eventtrackerapp/utils/TextField.kt
@@ -36,3 +36,34 @@ fun EventTrackerAppOutlinedTextField(txt:String,
     )
 
 }
+
+
+@Composable
+fun EventTrackerAppAuthTextField(
+    txt: String,
+    state: MutableState<String>,
+    leadingIcon: @Composable (()->Unit)? = null,
+    trailingIcon: @Composable (()->Unit)? = null,
+    isSingleLine: Boolean = true,
+    isReadOnly: Boolean = false,
+    isPassword: Boolean = false,
+    isError: Boolean = false,
+    onValueChange: (String)->Unit
+)
+{
+    OutlinedTextField(
+        modifier = Modifier.widthIn(max = 280.dp),
+        value = state.value,
+        onValueChange = onValueChange,
+        singleLine = isSingleLine,
+        readOnly = isReadOnly,
+        label = { Text(text = txt) },
+        leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon,
+        isError = isError,
+        visualTransformation = if(isPassword)
+            PasswordVisualTransformation()
+        else VisualTransformation.None
+    )
+
+}
