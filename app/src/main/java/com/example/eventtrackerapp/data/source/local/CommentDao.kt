fec955561ed271c5d62485b455f7e86895892312
Yavuz Selim Ozkan <ozkanyavuzselim99@gmail.com>
2025-07-11 15:38:59 +0300

    feat:integrated comment repository

--- app/src/main/java/com/example/eventtrackerapp/data/source/local/CommentDao.kt
+++ app/src/main/java/com/example/eventtrackerapp/data/source/local/CommentDao.kt
@@ -2,6 +2,7 @@ package com.example.eventtrackerapp.data.source.local
 
 import android.security.identity.AccessControlProfileId
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -15,32 +16,13 @@ interface CommentDao {
     @Insert
     suspend fun insertComment(comment: Comment)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAllComments(comments:List<Comment>)
-
-    @Query("SELECT * FROM comments")
-    fun getAllComment():Flow<List<Comment>>
-
     @Transaction
     @Query("SELECT * FROM comments WHERE eventId = :eventId")
     fun getCommentsForEvent(eventId:String):Flow<List<Comment>>
 
-    @Transaction
-    @Query("SELECT * FROM comments WHERE eventId = :eventId")
-    fun getCommentForEventWithDetail(eventId: String):Flow<List<CommentWithProfileAndEvent>>
-
-    @Query("SELECT * FROM comments WHERE id = :commentId")
-    suspend fun getCommentByIdOnce(commentId: String):Comment
-
-    @Query("SELECT * FROM comments WHERE eventId = :eventId")
-    suspend fun getCommentsForEventOnce(eventId: String):List<Comment>
-
-    @Query("SELECT * FROM comments WHERE profileId = :profileId")
-    suspend fun getCommentsForProfileOnce(profileId: String):List<Comment>
-
     @Query("SELECT COUNT(*) FROM comments WHERE eventId = :eventId")
     fun getCommentCount(eventId: Int):Flow<Int>
 
-    @Query("SELECT * FROM comments WHERE id = :commentId")
-    fun getCommentById(commentId: String):Flow<Comment>
+    @Query("DELETE FROM comments WHERE eventId = :eventId")
+    suspend fun deleteCommentsForEvent(eventId: String)
 }
